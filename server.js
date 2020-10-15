d3caf9d69822f37b4bf7a8b514490582a8febe6e
vinc86 <vincenzomancuso86@gmail.com>
2020-10-15 14:46:36 +0200

    done - no bonus

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json({extended: false}))
+
+
 app.get('/getme', (req, res) => {
-  res.send(fruits)
+  res.send({message: `${req.url} path`})
 });
+
+app.get("/fuckup/saw-it-coming",(req,res,next)=>{
+  try{
+    res.sen("hello")
+  }catch(err){
+    next(err)
+  }
+})
+
+app.get("/fuckup/surprise",(req,res)=>{
+  throw new Error("There is a dummy error!")
+
+})
 
 app.post('/postme', (req, res) => {
   res.send({})
+})
+
+
+// 404 MIDDLEWARE
+app.use((req,res,next)=>{
+    res.status(404).json({error: `Path ${req.url} does not exist...`})
+})
+
+// GENERIC ERROR MIDDLEWARE
+app.use((err,req,res,next)=>{
+  res.status(404).json({error: err.message})
 })
 
 app.listen(5000, () => {
